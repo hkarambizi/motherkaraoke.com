c9bde9e7c94b1131f8dd51697152a7368b5b0962
Rene Dellefont <renedellefont@gmail.com>
2016-12-25 11:33:49 -0500

    Update styles delete path

--- gulp/tasks/clean:styles.js
+++ gulp/tasks/clean:styles.js
@@ -1,5 +1,5 @@
 module.exports = function(gulp, plugins, config) {
 	return function() {
-		return plugins.del([config.styles.jekylldest + 'styles.min.css']);
+		return plugins.del([config.styles.appdest + '/styles.min.css']);
   };
 };
