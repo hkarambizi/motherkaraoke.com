0c72fbfca5ba245bd74f42c5e3fcdba64500d08b
Rene <renedellefont@gmail.com>
2017-06-06 11:55:46 -0400

    Updates gulpfile to 4.0

---
+++ gulpfile.babel.js
@@ -0,0 +1,39 @@
+/* GULP PLUGINS
+********** */
+import gulp from 'gulp';
+import gutil from 'gulp-util';
+
+/* tasks
+********** */
+import images from './gulp/tasks/images';
+import styles from './gulp/tasks/styles';
+import scripts from './gulp/tasks/scripts';
+import svg from './gulp/tasks/svg';
+import clean from './gulp/tasks/clean';
+import { jekyll, jekyllserve } from './gulp/tasks/jekyll';
+import * as server from './gulp/tasks/browsersync';
+import watch from './gulp/tasks/watch';
+
+/* config
+********** */
+const env = gutil.env.env || 'development';
+
+const build = gulp.series(
+  clean,
+  gulp.parallel(images, styles, scripts, svg, jekyll),
+);
+
+const serve = gulp.series(
+  clean,
+  gulp.parallel(images, styles, scripts, svg, jekyllserve),
+  server.serverStart,
+  watch,
+);
+
+const def = (done) => {
+  console.log(gutil.env.production); // true when --production passed
+  console.log(env); // development
+  done();
+};
+export { serve, build };
+export default def;
