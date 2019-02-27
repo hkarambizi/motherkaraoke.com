23ac122202c8db40429df3aef2b4567af8217101
hkarambizi <hkarambizi@gmail.com>
2019-02-26 20:59:07 -0500

    debug after cloning again

--- _assets/scripts/az-search.js
+++ _assets/scripts/az-search.js
@@ -5,17 +5,18 @@ import PubSub from "./pubsub";
 import * as api from "./api";
 
 // Initialize Firebase
-var config = FIREBASE_CONFIG;
+// var config = FIREBASE_CONFIG;
 
 // TO-DO: Controll access to secure variables
-// var config = {
-//   apiKey: process.env.FIREBASE_API_KEY,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   databaseURL: process.env.FIREBASE_DB_URL,
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MSG_SENDER_ID
-// };
+var config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DB_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MSG_SENDER_ID
+};
+
 firebase.initializeApp(config);
 
 // Initialize Cloud Firestore through Firebase
